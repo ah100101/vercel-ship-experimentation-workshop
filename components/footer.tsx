e65cbf843b5845042dd299f9c4348a55ab5a1ade
Alex <alex.hawley@vercel.com>
2024-04-15 14:52:25 -0500

    removed dark

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,7 +14,6 @@ export default function Footer() {
           <Image
             src="/vercel.svg"
             alt="Vercel Logo"
-            className="dark:invert"
             width={100}
             height={24}
             priority
--- components/product-hero.tsx
+++ components/product-hero.tsx
@@ -22,11 +22,11 @@ export default function ProductHero(props: {
           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
             {title}
           </h1>
-          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
             {description}
           </p>
           <Link
-            className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+            className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
             href="#"
           >
             View Product
