ecdd0e7a55d3a61e2e79f4cda19c5393278a29e7
Alex <alex.hawley@vercel.com>
2024-04-25 15:35:44 -0500

    enabled ppr

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    ppr: true,
+  }
+};
 
 import withVercelToolbar from "@vercel/toolbar/plugins/next";
 export default withVercelToolbar()(nextConfig);
