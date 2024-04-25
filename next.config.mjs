185e010e86b32742fa2418eeadd9c3607bcb78d2
Alex <alex.hawley@vercel.com>
2024-04-25 15:38:42 -0500

    Revert "enabled ppr"
    This reverts commit ecdd0e7a55d3a61e2e79f4cda19c5393278a29e7.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    ppr: true,
-  }
-};
+const nextConfig = {};
 
 import withVercelToolbar from "@vercel/toolbar/plugins/next";
 export default withVercelToolbar()(nextConfig);
